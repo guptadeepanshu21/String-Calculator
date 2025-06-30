bbb09566a33e837aeed80884b8806f58f5080c93
Deepanshu Gupta <guptadeepanshu21@gmail.com>
2025-07-01 00:09:11 +0530

    add main file to start application

---
+++ main.js
@@ -0,0 +1,9 @@
+const { Add } = require("./src/Add");
+
+console.log(Add(""));
+console.log(Add("1"));
+console.log(Add("1,5"));
+console.log(Add("1\n2,3"));
+console.log(Add("//;\n1;2"));
+
+// Add your function calls here
