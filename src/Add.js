08ce771e996151acc82cf4965aab76970dae065f
Deepanshu Gupta <guptadeepanshu21@gmail.com>
2025-06-30 21:36:20 +0530

    handle invalid inputs

--- src/Add.js
+++ src/Add.js
@@ -1,5 +1,9 @@
-const Add = () => {};
+const Add = (input) => {
+  if (typeof input !== "string") {
+    throw "non-string input is not allowed";
+  }
+};
 
 module.exports = {
-    Add
-}
+  Add,
+};
--- tests/Add.spec.js
+++ tests/Add.spec.js
@@ -1,8 +1,12 @@
-const {expect, test} = require('@jest/globals');
-const {Add} =  require('../src/Add.js');
+const { expect, test } = require("@jest/globals");
+const { Add } = require("../src/Add.js");
 
-describe('Add', () => {
-    test('Add method exists', () => {
-        expect(typeof Add).toBe('function');    
-    });
-})
+describe("Add", () => {
+  test("Add method exists", () => {
+    expect(typeof Add).toBe("function");
+  });
+
+  test("should return exception for non-string input", () => {
+    expect(() => Add(null)).toThrow("non-string input is not allowed");
+  });
+});
